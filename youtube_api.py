a1616a61e9729b2ab91cc6dd545a916c943113ec
sachin-101 <sachinkumar04428@gmail.com>
2019-11-15 10:52:09 +0530

    basic youtube comment scraper added
    Extracts comments from youtube based on the keyword passed and saves them in a .csv file

---
+++ youtube_api.py
@@ -0,0 +1,89 @@
+import os
+import pickle
+import pandas as pd
+import google.oauth2.credentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+CLIENT_SECRET_FILE = "client_secret.json"
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_autenticated_service():
+    credentials = None
+    # if file exists
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    # Else if it is invalid or does not exist
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            credentials = flow.run_console()
+    
+        # save the credentials
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(credentials, token)
+
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+def get_videos(service, **kwargs):
+    final_results = []
+    results = service.search().list(**kwargs).execute()
+    
+    i = 0
+    max_pages = 3
+    while results and i<max_pages:
+        final_results.extend(results['items'])
+
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']  # update current page token, and add it to kwargs
+            results = service.search().list(**kwargs).execute()
+            i += 1
+        else:
+            break
+    
+    return final_results
+
+def extract_comments_by_video_keyword(service, **kwargs):
+    results = get_videos(service, **kwargs)
+    final_results = [] 
+    for item in results:
+        title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        comments = get_video_comments(service, part='snippet', videoId=video_id, textFormat='plainText')
+        final_results.extend([(video_id, title, comment) for comment in comments])
+    return final_results
+
+def get_video_comments(service, **kwargs):
+    comments = []
+    results = service.commentThreads().list(**kwargs).execute()
+
+    while results:
+        for item in results['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments.append(comment)
+    
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']  # update current page token, and add it to kwargs
+            results = service.commentThreads().list(**kwargs).execute()
+        else:
+            break
+        
+    return comments
+
+
+if __name__=='__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    service = get_autenticated_service()
+    keyword = input('Enter a keyword: ')
+    all_comments = extract_comments_by_video_keyword(service, q=keyword, part='id,snippet', eventType='completed', type='video')
+    data = pd.DataFrame(all_comments)
+    data.to_csv(f'{keyword}_comments.csv')
